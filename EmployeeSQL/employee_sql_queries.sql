e1761d05ca17845c104b0dec5d264875aed3617e
unknown <christian.reyes@northwestern.edu>
2021-08-25 23:46:22 -0500

    completed sql files

--- EmployeeSQL/employee_sql_queries.sql
+++ EmployeeSQL/employee_sql_queries.sql
@@ -1,6 +1,82 @@
--- List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT A.emp_no,A.last_name,A.first_name,A.sex,B.salary FROM employees AS A 
+--List the tables for each of the six CSV files
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT A.emp_no,A.last_name,A.first_name,A.sex,B.salary 
+FROM employees AS A 
 LEFT JOIN salaries AS B 
 ON A.emp_no=B.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date FROM employees
+WHERE date_part('year', hire_date) = '1986';
+
+--List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+SELECT A.dept_no, B.dept_name, A.emp_no, C.last_name, C.first_name
+FROM dept_manager AS A
+LEFT JOIN departments AS B
+ON A.dept_no=B.dept_no  
+LEFT JOIN employees AS C
+ON A.emp_no=C.emp_no;
+
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+SELECT A.emp_no,B.last_name,B.first_name,C.dept_name
+FROM dept_emp AS A
+LEFT JOIN employees AS B
+ON A.emp_no=B.emp_no
+LEFT JOIN departments AS C
+ON A.dept_no=C.dept_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex FROM employees
+Where first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, 
+--including their employee number, last name, first name, and department name.
+SELECT A.emp_no,B.last_name,B.first_name,C.dept_name
+FROM dept_emp AS A
+LEFT JOIN employees AS B
+ON A.emp_no=B.emp_no
+LEFT JOIN departments AS C
+ON A.dept_no=C.dept_no
+WHERE C.dept_name='Sales';
+
+--List all employees in the Sales and Development departments
+--including their employee number, last name, first name, and department name.
+SELECT A.emp_no,B.last_name,B.first_name,C.dept_name
+FROM dept_emp AS A
+LEFT JOIN employees AS B
+ON A.emp_no=B.emp_no
+LEFT JOIN departments AS C
+ON A.dept_no=C.dept_no
+WHERE C.dept_name='Sales'
+OR C.dept_name='Development';
+
+--In descending order, list the frequency count of employee last names, 
+--i.e., how many employees share each last name.
+SELECT last_name, COUNT(*) FROM employees
+GROUP BY last_name 
+ORDER BY COUNT(*) DESC;
+
+--'Search your ID number'
+SELECT A.emp_no,A.emp_title_id,B.title,A.birth_date,A.first_name,A.last_name,A.sex,A.hire_date,C.dept_no,D.dept_name,E.salary
+FROM employees AS A
+LEFT JOIN titles AS B
+ON A.emp_title_id=B.title_id
+LEFT JOIN dept_emp AS C
+ON A.emp_no=C.emp_no
+LEFT JOIN departments AS D
+ON C.dept_no=D.dept_no
+LEFT JOIN salaries as E
+ON A.emp_no=E.emp_no
+WHERE A.emp_no='499942';
 
 
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# sql-challenge
+# SQL-Challenge
+
+## Employee Database: A Mystery in Two Parts
+
+It is a beautiful spring day, and it is two weeks since you have been hired as a new data engineer at Pewlett Hackard. Your first major task is a research project on employees of the corporation from the 1980s and 1990s. All that remain of the database of employees from that period are six CSV files.
+
+In this assignment, you will design the tables to hold data in the CSVs, import the CSVs into a SQL database, and answer questions about the data. In other words, you will perform:
+
+* Data Modeling and Data Engineering
+
+* Data Analysis
+
+-----
+
+# Data Modeling
+
+![alt text](http://url/to/img.png)
+
+-----
+
+# Set up jupyter notebook
+
+~~~~sql
+--List the tables for each of the six CSV files
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+~~~~
